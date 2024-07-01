6bccec95cfe4bf12db1eb915cdca82c51ba888cd
Thomas Truong <thomas.truong@helloself.com>
2024-07-01 14:58:25 +0100

    feat(task): adding instructions for the task
    * Adding screenshots for board
    * Adding further instructions of the task
    * Adding initial skeleton code

--- README.md
+++ README.md
@@ -1,36 +1,53 @@
 # Take Home Exercise
+
 Thank you for taking the time to do this exercise. Please spend no longer than an hour on the below. If you enjoy the excercise and would like to continue, we would be delighted to interview. We will spend the first part of your interview discussing this work.
 
-Please fork this repository and send us your answer. Remember to provide us relevant permission to access the repository. 
+Please fork this repository and send us your answer. Remember to provide us relevant permission to access the repository.
 
 ## Brief: Trello Board Bookings
+
 At HelloSelf, our operations team have to manage new bookings. We have a form to enter the member's details:
+
 - Name
 - Title
-- Age 
+- Age
 - Email
 - Mobile Number
 
-The board has 3 columns:
-- **New**: pending new cases
-- **Triage**: looking for corresponding Therapist
-- **Matched**: matched with Therapist
-A card cannot move from New -> Matched before going to Triage, same as Matched cannot move back to New. They can only move 1 column at a time.
+The board has 4 columns:
 
-Here is the design of the Trello Board:
+- **Unclaimed**: new cases
+- **First Contact**: operations team contact member
+- **Preparing Work Offer**: searching for Therapist
+- **Send to Therapist**: matched with Therapist
 
+This is what the board currenlty looks like:
+![initial-board](public/initial-board.png)
 
-## Task
-1. Please add validations to the member form, and relevant error and helper messages
-2. Once 'Submit' is pressed, a card will be created on under New column
-3. Please save new card to *local storage* 
-4. Previous entered cards will reload onto the board as saved in *local storage*
-5. Please add styling to the form
+The final design of the board is:
+![final-board](public/final-board.png)
 
-## Bonus: Task
-1. Implement the drag and drop ability of the cards, where it must follow the rules stated above
-2. Card must indicate the current status/column they are in, feel free to use any kind of indicator
-3. Please add styling to the board
+## Tasks
+
+1. Please add validations to the member form, and relevant error and helper messages.
+2. Once 'Submit' is pressed, a card will be created on under _Unclaimed_ column.
+3. The card will contain all the details requested from the form.
+4. Please save new card to _local storage_.
+5. Previous entered cards will reload onto the board as saved in _local storage_.
+6. Please add styling to the form and page.
+
+## Bonus: Tasks
+
+If you have time and feeling adventurous feel free to implement the bonus tasks listed below.
+
+1. Implement the drag and drop of the cards to different columns.
+2. Each column must indicate the number of cards.
+3. Please add styling to the board and page.
 
 ## Review Interview
-Once we have received your repository we will organise a follow-up interview to discuss your answers. =
+
+Once we have received your repository we will organise a follow-up interview to discuss your solution.
+
+## How to run
+
+We can created a simple App using create-react-app and the instructions on how to run can be found in [here](CONTRIBUTING.md).
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,123 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <b>HelloSelf Trello Board</b>
       </header>
+
+      <div className="App-content">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            margin: "30px",
+            gap: "15px",
+          }}
+        >
+          <b>Form</b>
+          <div>
+            <label htmlFor="title">Title:</label>
+            <input type="text" id="title" name="title" />
+          </div>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" />
+          </div>
+          <div>
+            <label htmlFor="age">Age:</label>
+            <input type="text" id="age" name="age" />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input type="text" id="email" name="email" />
+          </div>
+          <div>
+            <label htmlFor="phone">Phone:</label>
+            <input type="text" id="phone" name="phone" />
+          </div>
+
+          <button>Submit</button>
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "100%",
+            textAlign: "center",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              height: "100%",
+              justifyContent: "space-between",
+              gap: "10px",
+            }}
+          >
+            <div
+              style={{
+                flexBasis: "100%",
+              }}
+            >
+              <b>Unclaimed</b>
+              <div
+                style={{
+                  background: "blue",
+                  border: "1px solid white",
+                  height: "100%",
+                }}
+              ></div>
+            </div>
+            <div
+              style={{
+                flexBasis: "100%",
+              }}
+            >
+              <b>First Contact</b>
+              <div
+                style={{
+                  background: "blue",
+                  border: "1px solid white",
+                  height: "100%",
+                }}
+              ></div>
+            </div>
+            <div
+              style={{
+                flexBasis: "100%",
+              }}
+            >
+              <b>Preparing Work Offer</b>
+              <div
+                style={{
+                  background: "blue",
+                  border: "1px solid white",
+                  height: "100%",
+                }}
+              ></div>
+            </div>
+            <div
+              style={{
+                flexBasis: "100%",
+              }}
+            >
+              <b>Send to Therapists</b>
+              <div
+                style={{
+                  background: "blue",
+                  border: "1px solid white",
+                  height: "100%",
+                }}
+              ></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
